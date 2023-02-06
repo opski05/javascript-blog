cde4eef765d67aa32d68a263999ef8b4972defec
Oskar Pozorski <oskar.pozorski@gmail.com>
2023-02-06 15:37:52 +0100

    Add articleSelector, targetArticle and class active to targetArticle

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
   });*/
 
   const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
@@ -26,10 +27,13 @@
     }
 
     /* get 'href' attribute from the clicked link */
-  
+    const articleSelector = clickedElement.getAttribute('href');
+    
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    const targetArticle = document.querySelector(articleSelector);
+    
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
